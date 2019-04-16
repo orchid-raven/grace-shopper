61a885712aa3c7396c3ae79a6c1c428f386f3d8a
Jan Czarina Javier <janczarinajavier@Jans-MacBook-Pro.local>
2019-04-16 12:22:41 -0400

    created ProductsByType backend

---
+++ client/components/AllProductsByType.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class AllProductsByType extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>All Item Types Here</div>
+    )
+  }
+}
+
+export default AllProductsByType;
--- server/api/products.js
+++ server/api/products.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const {Product} = require('../db/models')
 module.exports = router
 
+//GetAll
 router.get('/', async (req, res, next) => {
   try {
     const products = await Product.findAll()
@@ -9,4 +10,16 @@ router.get('/', async (req, res, next) => {
   } catch (err) {
     next(err)
   }
-})
+});
+
+// GetItemTypes
+router.get('/:ItemType', async (req, res, next) => {
+  try {
+    const allItems = await Product.findAll({ where: {
+      productType: req.params.ItemType
+    }});
+    res.json(allItems);
+  } catch (err) {
+    next(err)
+  }
+});
