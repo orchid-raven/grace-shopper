fe96fec6f23ceabebbe5f39046649a8a10ca837c
jedesai <jeet.d.desai@gmail.com>
2019-04-18 15:11:44 -0400

    fixxing merger conflict

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,28 +6,6 @@ import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
-<<<<<<< HEAD
-    {/* <h1>BOILERMAKER</h1> */}
-    <nav>
-      <div>
-        {isLoggedIn ? (
-          <div className="left">
-            {/* The navbar will show these links after you log in */}
-            <a href="#">Logo</a>
-            <Link to="/home">Home</Link>
-            <a href="#" onClick={handleClick}>
-              Logout
-            </a>
-          </div>
-        ) : (
-          <div>
-            {/* The navbar will show these links before you log in */}
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Sign Up</Link>
-          </div>
-        )}
-      </div>
-=======
     <nav>
       <Link to="/home">
         <img src="/images/logo/Orchid-Raven-Logo.png" />
@@ -54,7 +32,6 @@ const Navbar = ({handleClick, isLoggedIn}) => (
           <Link to="/cart">Cart<i className="fas fa-shopping-cart"></i></Link>
         </div>
       )}
->>>>>>> master
     </nav>
   </div>
 )
