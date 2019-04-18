1085f691c51e397b718fcb7dd7431cd7869a93c0
jedesai <jeet.d.desai@gmail.com>
2019-04-18 12:40:14 -0400

    done with home and working on styling

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -3,31 +3,28 @@ import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
-import 'materialize-css/dist/css/materialize.min.css'
-//  import M from 'materialize-css/dist/js/materialize.min.js'
-import M from 'materialize-css'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
-  <div className='container'>
+  <div>
     {/* <h1>BOILERMAKER</h1> */}
     <nav>
-      <div className='nav-wrapper'>
-      {isLoggedIn ? (
-        <div className='left'>
-          {/* The navbar will show these links after you log in */}
-          <a href="#" className=''><img src='/images/logo/orchidRavenLogo.png'/></a>
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )}
+      <div>
+        {isLoggedIn ? (
+          <div className="left">
+            {/* The navbar will show these links after you log in */}
+            <a href="#">Logo</a>
+            <Link to="/home">Home</Link>
+            <a href="#" onClick={handleClick}>
+              Logout
+            </a>
+          </div>
+        ) : (
+          <div>
+            {/* The navbar will show these links before you log in */}
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Sign Up</Link>
+          </div>
+        )}
       </div>
     </nav>
     <hr />
