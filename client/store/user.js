57b4cc2161373584175ae36adb6d911a88bbbed6
Jan Czarina Javier <janczarinajavier@Jans-MacBook-Pro.local>
2019-04-20 23:12:12 -0400

    added error handling for logout

--- client/store/user.js
+++ client/store/user.js
@@ -57,8 +57,14 @@ export const auth = (email, password, method) => async dispatch => {
 }
 
 export const logout = () => async dispatch => {
+
   try {
     await axios.get('/api/cart/archiveCart')
+  }
+  catch (error){
+    console.log(error);
+  }
+  try {
     await axios.post('/auth/logout')
     dispatch(removeUser())
     history.push('/login')
