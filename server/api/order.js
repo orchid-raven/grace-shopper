698d84399f25e0a25fda92eacb37e696c2dc77f9
Jan Czarina Javier <janczarinajavier@Jans-MacBook-Pro.local>
2019-04-22 12:51:26 -0400

    Initialized routes for Order Receipt / Order History

--- server/api/order.js
+++ server/api/order.js
@@ -4,6 +4,13 @@ const Product = require('../db/models/product');
 
 module.exports = router
 
+router.get('/History', async (req, res, next) => {
+  let history = await Order.findAll({
+    include: [{model: Product}]
+  });
+  res.json(history);
+});
+
 router.get('/:orderId', async (req, res, next) => {
   let newOrder = await Order.findAll({
     where:{
@@ -14,3 +21,10 @@ router.get('/:orderId', async (req, res, next) => {
   res.json(newOrder);
 });
 
+router.delete('/ClearHistory', async (req, res, next) => {
+  let history = await Order.findAll({
+    include: [{model: Product}]
+  });
+  await history.destroy();
+  res.send();
+})
