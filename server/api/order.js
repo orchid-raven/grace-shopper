944696c6acc4aa7bfd194db9c7b0409f2898b12c
Jan Czarina Javier <janczarinajavier@Jans-MacBook-Pro.local>
2019-04-22 12:43:43 -0400

    Initialized routes for Order Receipt / Planned Receipt Front-End

---
+++ server/api/order.js
@@ -0,0 +1,16 @@
+const router = require('express').Router()
+const Order = require('../db/models/order')
+const Product = require('../db/models/product');
+
+module.exports = router
+
+router.get('/:orderId', async (req, res, next) => {
+  let newOrder = await Order.findAll({
+    where:{
+    id: req.params.orderId
+  },
+    include: [{model: Product}]
+  });
+  res.json(newOrder);
+});
+
